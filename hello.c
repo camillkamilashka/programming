00a84f310a78e57439d4cf002cb3ed7aa490032e
user <user@ws1013402129.pm>
2023-09-18 16:01:06 +0500

    programming

--- hello.c
+++ hello.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
-    printf("Hello,world!\n");
+    float a, b;
+     printf("Enter a -> ");
+    scanf("%f", &a);
+    printf("Enter b -> ");
+    scanf("%f", &b);
+    s = a+a 
+    if (!a && !b)
+        return 0;
+
     return 0;
 }
