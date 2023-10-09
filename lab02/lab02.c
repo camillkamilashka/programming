742af5e294008a64d3fbbb348fb138a771ab84fa
user <user@ws1013402129.pm>
2023-10-09 13:57:02 +0500

    wip lab02

---
+++ lab02/lab02.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    double x, y, s;
+    x = 0.0; 
+    scanf("%lf", &s);
+    int n = 2 / s + 1;
+    while (n) 
+   {
+        if (x >= -1 && x <= 1)
+          y = exp(-2*sin(x));
+        if (x > 1 && x <= 2)
+          y = x*x - cos(x)/sin(x);
+         printf("%f %f\n", x, y);
+        x += s;
+        n = n-1;
+   }
+
+}
+
---
+++ lab02/labfor02
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    double x, y, s;
+    x = 0.0; 
+    scanf("%lf", &s);
+    int n, i;
+    for (i = 0; i <= n; i++)  
+    {
+        if (x >= -1 && x <= 1)
+          y = exp(-2*sin(x));
+        if (x > 1 && x <= 2)
+          y = x*x - cos(x)/sin(x);
+         printf("%f %f\n", x, y);
+        x += s;
+    }
+
+}
